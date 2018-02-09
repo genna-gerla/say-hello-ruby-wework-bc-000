1133288882609b07fdd6a3fade19747230402738
Genna Gerla <genna.gerla@wework.com>
2018-02-09 00:46:06 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,7 @@
 # Build your say_hello method here
 # I call on the method, say_hello, and give it the string "Gabriela" 
 def say_hello(name)
+  puts "Hello #{name}!"
 phrase = "Hello "
 puts phrase
 end
